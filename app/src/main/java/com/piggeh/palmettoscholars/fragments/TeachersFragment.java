03b2a588d4ac6792ced82711398206d9a420262e
Peter <peter@glaabs.com>
2016-09-14 14:45:25 -0400

    Added subheader to Teacher layout

--- app/src/main/java/com/piggeh/palmettoscholars/fragments/TeachersFragment.java
+++ app/src/main/java/com/piggeh/palmettoscholars/fragments/TeachersFragment.java
@@ -7,11 +7,17 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.piggeh.palmettoscholars.R;
 import com.piggeh.palmettoscholars.adapters.TeachersRecyclerAdapter;
 import com.piggeh.palmettoscholars.classes.RecyclerItemDivider;
@@ -26,13 +32,15 @@ import java.util.Arrays;
  */
 public class TeachersFragment extends Fragment
 implements TeachersRecyclerAdapter.RecyclerItemClickListener {
-    private static final String TAG = "ContactFragment";
+    private static final String TAG = "TeachersFragment";
 
     private OnTeacherClickListener mListener;
 
     private RecyclerView recyclerView;
     private ProgressBar progressBarLoadingTeachers;
     private TeachersRecyclerAdapter recyclerAdapter;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     public TeachersFragment() {
         // Required empty public constructor
@@ -62,6 +70,25 @@ implements TeachersRecyclerAdapter.RecyclerItemClickListener {
             *//*mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);*//*
         }*/
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference("teachers");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
         recyclerAdapter = new TeachersRecyclerAdapter(getTeachersFromIndex(),
                 null,
                 getContext(),
