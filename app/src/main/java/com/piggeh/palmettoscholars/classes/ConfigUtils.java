b04f83c994d108d98a7594791e29b867c4660d71
Peter <peter@glaabs.com>
2016-09-14 11:45:35 -0400

    Added hasTouch to ConfigUtils

--- app/src/main/java/com/piggeh/palmettoscholars/classes/ConfigUtils.java
+++ app/src/main/java/com/piggeh/palmettoscholars/classes/ConfigUtils.java
@@ -1,14 +1,15 @@
 package com.piggeh.palmettoscholars.classes;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 
 import com.piggeh.palmettoscholars.R;
 
-/**
- * Created by peter on 9/12/2016.
- */
 public class ConfigUtils {
     public static boolean isLarge(Context context){
         return context.getResources().getBoolean(R.bool.configuration_islarge);
+    }
+    public static boolean hasTouch(Context context){
+        return context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_TOUCHSCREEN);
     }
 }
